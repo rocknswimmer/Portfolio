47a08b3e0536b480f2912d7b264e061617f3c441
Andy Simmons <rocknswimmer116@gmail.com>
2023-01-16 13:48:01 -0500

    links and general set up done now for a bit of style before deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ function App() {
 
   return (
     <div>
-      <h1>Site Title</h1>
+      <h1>Andy's Deployed Demos</h1>
+      <h2>Generic</h2>
+      <ul>
+        <li><a href="http://52.21.107.140:3004/">Job Site</a></li>
+      </ul>
+      <h2>Cards</h2>
+      <ul>
+        <li><a href="http://52.21.107.140:3001/">Classic</a></li>
+        <li><a href="http://52.21.107.140:3002/">Greetings Green</a></li>
+        <li><a href="http://52.21.107.140:3003/">For the Fam</a></li>
+      </ul>
+      <h2>Educational</h2>
+      <ul>
+        <li><a href="http://52.21.107.140:3005/">{'Modals and Accordions (laughter presentation) presentation'}</a></li>
+      </ul>
     </div>
   )
 }
